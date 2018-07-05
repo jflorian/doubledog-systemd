111e8edcf75978c3fb8b0b68f398220e51b434d9
John Florian <jflorian@doubledog.org>
2018-07-05 11:43:42 -0400

    Refactor - extract new Systemd::Flexsize data type
    Signed-off-by: John Florian <jflorian@doubledog.org>

---
+++ types/flexsize.pp
@@ -0,0 +1,20 @@
+#
+# == Type: systemd::flexsize
+#
+# === Authors
+#
+#   John Florian <jflorian@doubledog.org>
+#
+# === Copyright
+#
+# This file is part of the doubledog-systemd Puppet module.
+# Copyright 2018 John Florian
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+type Systemd::Flexsize = Variant[
+    Integer[0],
+    Pattern[
+        /\A\d+[%KMGTPE]\Z/,
+    ],
+]
