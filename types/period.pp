9d07c9e72d5676ccbba5aa2458de965b5a85e84e
John Florian <jflorian@doubledog.org>
2018-07-05 11:43:42 -0400

    Refactor - extract new Systemd::Period data type
    Signed-off-by: John Florian <jflorian@doubledog.org>

---
+++ types/period.pp
@@ -0,0 +1,20 @@
+#
+# == Type: systemd::period
+#
+# === Authors
+#
+#   John Florian <jflorian@doubledog.org>
+#
+# === Copyright
+#
+# This file is part of the doubledog-systemd Puppet module.
+# Copyright 2018 John Florian
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+type Systemd::Period = Variant[
+    Integer[0],
+    Pattern[
+        /\A\d+(year|month|week|day|[hm])\Z/,
+    ],
+]
